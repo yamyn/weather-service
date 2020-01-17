37eb855f10fe25431509c2ae7bf5251a87c3efec
Yaroslav Myn <ricky.month@gmail.com>
2020-01-17 23:14:29 +0200

    Added meteorology data view

--- build-utils/development.config.js
+++ build-utils/development.config.js
@@ -1,30 +1,31 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => ({
-  devtool: "cheap-eval-source-map",
+  devtool: 'cheap-eval-source-map',
 
   devServer: {
+    port: 4040,
     compress: true,
-    stats: "errors-only",
+    stats: 'errors-only',
     historyApiFallback: true,
     quiet: true,
     noInfo: true,
     open: true,
-    clientLogLevel: "warning"
+    clientLogLevel: 'warning',
   },
 
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html"
-    })
-  ]
+      template: './index.html',
+    }),
+  ],
 });
--- src/js/weather-gen.js
+++ src/js/weather-gen.js
@@ -77,10 +77,28 @@ function getWeatherForThreePm(array) {
 
 function parsArrForFiveDaysLook(array) {
   const parsArray = array.map(elem => {
+    let phenomena = 'No precipitation';
+    let precValue;
+    if (elem.rain) {
+      phenomena = 'rain: ';
+      precValue = `${elem.rain['3h']} mm`;
+    } else if (elem.snow) {
+      phenomena = 'snow: ';
+      precValue = `${elem.snow['3h']} mm`;
+    }
+    const tempKelvin = elem.main.temp;
+    const tempCelsium = Math.round(tempKelvin - 273.15);
     return {
-      day: moment(elem.dt_txt).format('ddd'),
+      time: moment(elem.dt_txt).format('ddd'),
       alt: elem.weather[0].description,
       icon: elem.weather[0].icon,
+      tempCelsium,
+      humidity: elem.main.humidity,
+      cloudiness: elem.clouds.all,
+      wind: elem.wind.speed,
+      pressure: elem.main.pressure,
+      phenomena,
+      precValue,
     };
   });
 
